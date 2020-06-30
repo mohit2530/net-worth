b3e643feef436d751d9b55fe036d6cb28d7ab3ac
mohitpaudyal <mohit.paudyal@gmail.com>
2020-06-29 20:00:31 -0400

    updating the main dashboard

--- ui/src/components/Home/Home.js
+++ ui/src/components/Home/Home.js
@@ -1,21 +1,41 @@
 import React from 'react'
+import '../../util/home.scss'
+import Sidebar from './Sidebar'
 import auth from '../../auth/auth'
-import {useHistory} from 'react-router-dom'
-import {constants} from '../../util/constants'
+import { useHistory } from 'react-router-dom'
+import { Grid, Paper } from '@material-ui/core'
+import { constants } from '../../util/constants'
+import Typography from '@material-ui/core/Typography'
 
 const Home = (props) => {
     const route = constants.homeUrl,
         history = useHistory();
 
     return (
-        <div>
-            <h1> Home </h1>
-            <button onClick={ () => { 
-                auth.logout( () => { history.push(route) }) }}
-            >
-                {constants.logout}
-            </button>
-        </div>
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Paper>
+                        <div className = "top-navigation">
+                            <Typography
+                                variant="h4"
+                                color = "primary"
+                                className = "padding-top"
+                                >
+                                {constants.loginHeading}
+                            </Typography>
+                            <button className = "button" onClick={() => {
+                                auth.logout(() => { history.push(route) })
+                            }}
+                            >
+                                {constants.logout}
+                            </button>
+                        </div>
+                    </Paper>
+                </Grid>
+                    <Sidebar />
+            </Grid>
+        </>
     )
 }
 
--- ui/src/components/Home/Sidebar.js
+++ ui/src/components/Home/Sidebar.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import '../../util/home.scss'
 import FaceIcon from '@material-ui/icons/Face'
 import InfoIcon from '@material-ui/icons/Info'
 import {constants } from '../../util/constants'
+import { Paper } from '@material-ui/core'
 import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined'
 import ToggleOffOutlinedIcon from '@material-ui/icons/ToggleOffOutlined'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined'
 import EnhancedEncryptionOutlinedIcon from '@material-ui/icons/EnhancedEncryptionOutlined'
-import { Drawer, makeStyles, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { Drawer, makeStyles, List, ListItem, ListItemIcon, ListItemText, Container, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
     drawerPaper: { width: 'inherit', marginTop: 'inherit' },
-    link : {textDecoration : 'none', color: theme.palette.text.primary}
+    link : { textDecoration : 'none', color: theme.palette.text.primary},
+    container : {marginLeft: '1vh'},
+    paper : { margin: '10px', padding : '10px'}
 }));
 
 const Sidebar = () => {
@@ -89,6 +93,41 @@ const Sidebar = () => {
                             </Link>
                         </List>
                     </Drawer>
+                    <Switch>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                <Paper className = {classes.paper}>
+                                    {constants.home}
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                </Paper>
+                            </Container>
+                        </Route>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                {constants.home}
+                            </Container>
+                        </Route>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                {constants.home}
+                            </Container>
+                        </Route>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                {constants.home}
+                            </Container>
+                        </Route>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                {constants.home}
+                            </Container>
+                        </Route>
+                        <Route exact path = {constants.homeUrl}>
+                            <Container maxWidth="lg" className = {classes.container} >
+                                {constants.home}
+                            </Container>
+                        </Route>
+                    </Switch>
                 </div>
             </Router>
         </>
